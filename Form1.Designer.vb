7657f123c8c3d9b64797c4b2f195e6ed9aee883b
Aleksandr <a.korbut72@gmail.com>
2018-02-07 16:36:49 +0000

    Insert solution

---
+++ Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿Imports System.IO
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(675, 533)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(103, 27)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(38, 26)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(238, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(41, 67)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(234, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(43, 111)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(717, 361)
+        Me.RichTextBox1.TabIndex = 3
+        Me.RichTextBox1.Text = ""
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(536, 534)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(122, 25)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(819, 584)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,108 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim ComSFT(16) As String
+        Dim PathFtp, PathArh, Str1, NameFile, PathLog, login, password, server, port, hostkey As String
+        'Const pr As Char = "%"
+        Const dcav As Char = Chr(34)
+
+        PathFtp = "c:\testFtp"   '"c:\testFtp\test1.txt"
+        NameFile = "test1.txt"
+        PathArh = "c:\testFtp\arh\%sdat%.txt"
+        PathLog = "C:\testFtp\WinSCP1.log"
+        login = "prog"
+        password = "3RBkJLzW"
+        server = "spb.root.autocrm.ru"
+        port = "14872"
+        hostkey = "ssh-rsa 2048 30:31:44:d4:00:2c:f3:7d:70:09:83:58:68:c9:68:c8"
+
+        Dim dd = New CreateBatFile
+        ComSFT = dd.CreateSM(PathFtp, NameFile, PathArh, PathLog, login, password, server, port, hostkey)
+
+
+
+
+        'ComSFT(0) = "@echo On"
+        'ComSFT(1) = "Set " & dcav & "sdat=%date% %time~0,-3%" & dcav
+        'ComSFT(2) = "Set " & dcav & "sdat=%date% %time~0,-3%" & dcav
+        'ComSFT(3) = "Set " & dcav & "sdat=%sdat:=.%" & dcav
+        'ComSFT(4) = "Set " & dcav & "sdat=%" & "sdat:=_%" & dcav
+        'ComSFT(5) = "copy /-y " & PathFtp & " " & PathArh   'c:\testFtp\test1.txt c:\testFtp\arh\%sdat%.txt
+        'ComSFT(6) = dcav & "C:\Program Files (x86)\WinSCP\WinSCP.com" & dcav & "^"
+        'ComSFT(7) = "/log=" & dcav & "C:\Program Files (x86)\WinSCP\WinSCP1.log" & dcav & " /ini=nul ^"
+        'ComSFT(8) = "/command ^"
+        'ComSFT(9) = dcav & "open sftp://prog:3RBkJLzW@spb.root.autocrm.ru:14872/ -hostkey=" & dcav & dcav & "ssh-rsa 2048 30:31:44:d4:00:2c:f3:7d:70:09:83:58:68:c9:68:c8" & dcav & dcav & "^"
+        'ComSFT(10) = dcav & "lcd c:\testftp" & dcav & "^"
+        'ComSFT(11) = dcav & "put *.txt" & dcav & "^"
+        'ComSFT(12) = dcav & "exit" & dcav
+        'ComSFT(13) = ""
+        'ComSFT(14) = "set WINSCP_RESULT=%ERRORLEVEL%"
+        'ComSFT(15) = "If %WINSCP_RESULT% equ 0 ( echo Success ) Else ( echo Error )"
+        'ComSFT(16) = "Exit /b %WINSCP_RESULT%"
+        ''If %WINSCP_RESULT% equ 0 (
+        ''  echo Success
+        '') else (
+        ''  echo Error
+        '')
+
+        ''Exit /b %WINSCP_RESULT%
+        ''"
+        ''        RichTextBox1.Text = $"Ghbdtn dfcz
+        ''{TextBox1.Text }^ 
+        ''{TextBox2.Text }^
+
+        ''"
+        ''" {0} {1}", TextBox1.Text,TextBox2.Text
+        For Each i As String In ComSFT
+
+            RichTextBox1.Text = RichTextBox1.Text & i & vbCrLf
+        Next
+
+
+
+    End Sub
+End Class
+Public Class CreateBatFile
+    Public Function CreateSM(ByVal _PathFtp As String,
+                             ByVal _NameFile As String,
+                             ByVal _PathArh As String,
+                             ByVal _PathLog As String,
+                             ByVal _login As String,
+                             ByVal _password As String,
+                             ByVal _server As String,
+                             ByVal _port As String,
+                             ByVal _hostkey As String
+                             ) As String()
+
+        Dim ComSFT(16) As String
+        'Dim PathFtp, PathArh As String
+        'Const pr As Char = "%"
+        Const dcav As Char = Chr(34)
+
+        'PathFtp = "c:\testFtp\test1.txt"
+        'PathArh = "c:\testFtp\arh\%sdat%.txt"
+
+        ComSFT(0) = "@echo Off"
+        ComSFT(1) = "set " & dcav & "sdat=%date% %time~0,-3%" & dcav
+        ComSFT(2) = "set " & dcav & "sdat=%date% %time~0,-3%" & dcav
+        ComSFT(3) = "set " & dcav & "sdat=%sdat:=.%" & dcav
+        ComSFT(4) = "set " & dcav & "sdat=%" & "sdat:=_%" & dcav
+        ComSFT(5) = "copy /-y " & _PathFtp & " " & _PathArh   'c:\testFtp\test1.txt c:\testFtp\arh\%sdat%.txt
+        ComSFT(6) = dcav & "C:\Program Files (x86)\WinSCP\WinSCP.com" & dcav & "^"
+        ComSFT(7) = "/log=" & dcav & _PathLog & dcav & " /ini=nul ^" '"C:\Program Files (x86)\WinSCP\WinSCP1.log
+        ComSFT(8) = "/command ^"
+        'ComSFT(9) = dcav & "open sftp://prog:3RBkJLzW@spb.root.autocrm.ru:14872/ -hostkey=" & dcav & dcav & "ssh-rsa 2048 30:31:44:d4:00:2c:f3:7d:70:09:83:58:68:c9:68:c8" & dcav & dcav & & dcav "^"
+        ComSFT(9) = dcav & "open sftp://" & _login & ":" & _password & "@" & _server & ":" & _port & "/ -hostkey=" & dcav & dcav & _hostkey & dcav & dcav & dcav & "^"
+        ComSFT(10) = dcav & "lcd " & _PathFtp & dcav & "^"
+        ComSFT(11) = dcav & "put " & _NameFile & dcav & "^"
+        ComSFT(12) = dcav & "exit" & dcav
+        ComSFT(13) = ""
+        ComSFT(14) = "set WINSCP_RESULT=%ERRORLEVEL%"
+        ComSFT(15) = "If %WINSCP_RESULT% equ 0 ( echo Success ) Else ( echo Error )"
+        ComSFT(16) = "Exit /b %WINSCP_RESULT%"
+
+        Return ComSFT
+
+    End Function
+
+End Class
+
